1a9a81f91ee9ea00bdfd0ad7bd774037eaa8b4e1
Muhammad Yaqoob <yaqoobm91@gmail.com>
2019-11-13 01:01:20 +0500

    Moved showAlert Component Effects to Alerts.js Component!

--- src/apps/AuthApp.js
+++ src/apps/AuthApp.js
@@ -14,15 +14,11 @@ import Room from "../components/pages/Room";
 
 import AuthContext from "../context/auth/authContext";
 import RoomsContext from "../context/rooms/roomsContext";
-import MaterialContext from "../context/material/materialContext";
-import AlertContext from "../context/alerts/alertContext";
 import ChatContext from "../context/chat/chatContext";
 
 const AuthApp = () => {
   const authContext = React.useContext(AuthContext);
   const roomsContext = React.useContext(RoomsContext);
-  const materialContext = React.useContext(MaterialContext);
-  const alertContext = React.useContext(AlertContext);
   const chatContext = React.useContext(ChatContext);
 
   // Loading Classrooms From Backend
@@ -33,84 +29,21 @@ const AuthApp = () => {
     // eslint-disable-next-line
   }, [authContext.loading]);
 
-  // Displaying Rooms Context Success Alerts!
+  // Setting Admin Property in Auth State
   React.useEffect(() => {
-    if (roomsContext.success) {
-      alertContext.showAlert("success", roomsContext.success);
-      roomsContext.clearSuccess();
-    }
-    // eslint-disable-next-line
-  }, [roomsContext.success]);
-
-  // Displaying Rooms Context Error Alerts!
-  React.useEffect(() => {
-    if (roomsContext.error) {
-      alertContext.showAlert("error", roomsContext.error);
-      roomsContext.clearError();
-    }
-    // eslint-disable-next-line
-  }, [roomsContext.error]);
-
-  // Displaying Auth Context Success Alerts
-  React.useEffect(() => {
-    if (authContext.success) {
-      alertContext.showAlert("success", authContext.success);
-      authContext.clearSuccess();
-    }
-    // eslint-disable-next-line
-  }, [authContext.success]);
-
-  // Displaying Auth Context Error Alerts
-  React.useEffect(() => {
-    if (authContext.error) {
-      alertContext.showAlert("error", authContext.error);
-      authContext.clearErrors();
-    }
-    // eslint-disable-next-line
-  }, [authContext.error]);
-
-  // Displaying Material Context Success Alerts
-  React.useEffect(() => {
-    if (materialContext.success) {
-      alertContext.showAlert("success", materialContext.success);
-      materialContext.clearSuccess();
-    }
-    // eslint-disable-next-line
-  }, [materialContext.success]);
-
-  // Displaying Material Context Error Alerts
-  React.useEffect(() => {
-    if (materialContext.error) {
-      alertContext.showAlert("error", materialContext.error);
-      materialContext.clearError();
-    }
-    // eslint-disable-next-line
-  }, [materialContext.error]);
-
-  React.useEffect(() => {
-    // If There is Room Set
     if (roomsContext.current) {
-      // Check & Set Admin
       if (authContext.user._id === roomsContext.current.createdBy) {
         authContext.setAdmin(true);
       }
-
-      // const room = {
-      //   roomid: roomsContext.current._id,
-      //   code: roomsContext.current.code
-      // };
-
-      // // Set Room in Chat
-      // chatContext.setRoom(room);
     }
 
     if (roomsContext.current === null) {
       authContext.setAdmin(false);
-      // chatContext.setRoom(null);
     }
     // eslint-disable-next-line
   }, [roomsContext.current]);
 
+  // Setting Logged in User in Chat State
   React.useEffect(() => {
     if (authContext.user) {
       const user = {
--- src/components/layout/Alerts.js
+++ src/components/layout/Alerts.js
@@ -1,22 +1,87 @@
-import React, { Fragment, useState, useContext, useEffect } from "react";
+import React, { Fragment } from "react";
 import { Snackbar } from "@material-ui/core";
 import AlertWrapper from "./AlertWrapper";
+
+// Contexts
 import AlertContext from "../../context/alerts/alertContext";
+import AuthContext from "../../context/auth/authContext";
+import RoomsContext from "../../context/rooms/roomsContext";
+import MaterialContext from "../../context/material/materialContext";
 
 const Alerts = () => {
-  const alertContext = useContext(AlertContext);
+  // Context
+  const alertContext = React.useContext(AlertContext);
+  const authContext = React.useContext(AuthContext);
+  const roomsContext = React.useContext(RoomsContext);
+  const materialContext = React.useContext(MaterialContext);
   const { alert, isAvailable } = alertContext;
 
-  const [open, setOpen] = useState(false);
-
+  // State
+  const [open, setOpen] = React.useState(false);
   const handleClick = () => setOpen(true);
   const handleClose = () => setOpen(false);
 
-  useEffect(() => {
+  // Component Effects
+  React.useEffect(() => {
     if (isAvailable) {
       handleClick();
     }
+    // eslint-dsiable-next-line
   }, [isAvailable]);
+
+  // Displaying Rooms Context Success Alerts!
+  React.useEffect(() => {
+    if (roomsContext.success) {
+      alertContext.showAlert("success", roomsContext.success);
+      roomsContext.clearSuccess();
+    }
+    // eslint-disable-next-line
+  }, [roomsContext.success]);
+
+  // Displaying Rooms Context Error Alerts!
+  React.useEffect(() => {
+    if (roomsContext.error) {
+      alertContext.showAlert("error", roomsContext.error);
+      roomsContext.clearError();
+    }
+    // eslint-disable-next-line
+  }, [roomsContext.error]);
+
+  // Displaying Auth Context Success Alerts
+  React.useEffect(() => {
+    if (authContext.success) {
+      alertContext.showAlert("success", authContext.success);
+      authContext.clearSuccess();
+    }
+    // eslint-disable-next-line
+  }, [authContext.success]);
+
+  // Displaying Auth Context Error Alerts
+  React.useEffect(() => {
+    if (authContext.error) {
+      alertContext.showAlert("error", authContext.error);
+      authContext.clearErrors();
+    }
+    // eslint-disable-next-line
+  }, [authContext.error]);
+
+  // Displaying Material Context Success Alerts
+  React.useEffect(() => {
+    if (materialContext.success) {
+      alertContext.showAlert("success", materialContext.success);
+      materialContext.clearSuccess();
+    }
+    // eslint-disable-next-line
+  }, [materialContext.success]);
+
+  // Displaying Material Context Error Alerts
+  React.useEffect(() => {
+    if (materialContext.error) {
+      alertContext.showAlert("error", materialContext.error);
+      materialContext.clearError();
+    }
+    // eslint-disable-next-line
+  }, [materialContext.error]);
 
   return (
     <Fragment>
