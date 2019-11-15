09002fe67e1d38eec487faae1e08a3577b0ffc1a
Muhammad Yaqoob <yaqoobm91@gmail.com>
2019-11-14 20:35:00 -0800

    Added Admin Badge Support in Message Avatar in Chat!

--- src/components/chat/MessageReceived.js
+++ src/components/chat/MessageReceived.js
@@ -1,12 +1,15 @@
 import React from "react";
 import {
   Avatar,
+  Badge,
   Paper,
   Divider,
   Typography,
   Grid,
   makeStyles
 } from "@material-ui/core";
+
+import RoomsContext from "../../context/rooms/roomsContext";
 
 const useStyles = makeStyles(theme => ({
   container: {
@@ -21,17 +24,37 @@ const useStyles = makeStyles(theme => ({
 }));
 
 const MessageReceived = ({ message }) => {
+  // Context
+  const roomsContext = React.useContext(RoomsContext);
+  const { current } = roomsContext;
+  const isAdmin = current.createdBy === message.user.id ? true : false;
+
   // Styles
   const classes = useStyles();
   const avatarURL = `/api/users/${message.user.id}/avatar`;
+
+  // Normal Avatar
+  const NormalAvatar = <Avatar src={avatarURL} />;
+
+  // Admin Avatar
+  const AdminAvatar = (
+    <Badge
+      color="secondary"
+      badgeContent="Admin"
+      anchorOrigin={{
+        horizontal: "right",
+        vertical: "top"
+      }}
+    >
+      <Avatar src={avatarURL} />
+    </Badge>
+  );
 
   return (
     <Grid container spacing={1} className={classes.container}>
       <Grid item xs={2} sm={1}>
         <Grid container justify="center">
-          <Grid item>
-            <Avatar src={avatarURL} />
-          </Grid>
+          <Grid item>{isAdmin ? AdminAvatar : NormalAvatar}</Grid>
         </Grid>
       </Grid>
 
